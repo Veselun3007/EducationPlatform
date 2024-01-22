2292eeddaa06f47a520753f81614f0d17f48ee88
Bohdan Fedyshen <fedyshenb@gmail.com>
2024-01-22 16:28:26 +0200

    Add TokenHelper.cs

---
+++ EducationPlatform.Identity/IdentityServer.Web/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer.Core.Interfaces;
+using IdentityServer.Web.DTOs.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Web.Controllers
+{
+    public class UserController(IBusinessUserOperation userOperation): Controller
+    {
+        private readonly IBusinessUserOperation _userOperation = userOperation;
+
+        [Route("updateUser/{id}")]
+        [HttpPut]
+        public async Task<IActionResult> UpdateMaterial(int id, UserDTO entity)
+        {
+            var user = await _userOperation.UpdateAsync(entity, id);
+
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [Route("deleteUser/{id}")]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteMaterial(int id)
+        {
+            await _userOperation.DeleteAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ EducationPlatform.Identity/IdentityServer.Web/Helpers/TokenHelper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace IdentityServer.Web.Helpers
+{
+    internal class TokenHelper
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _key;
+        private readonly string _accessTokenTtlInMinutes;
+
+        public TokenHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _key = _configuration["JWT:Key"] ?? throw new("JWT:Key");
+            _accessTokenTtlInMinutes = _configuration["JWT:AccessTokenTtlInMinutes"] ?? throw new("JWT:AccessTokenTtlInMinutes");
+        }
+
+        public static string GenerateRefreshToken()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        public string GenerateAccessToken(string name, string email)
+        {
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.Name, name),
+                new(ClaimTypes.Email, email)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_key));
+            int ttl = int.Parse(_accessTokenTtlInMinutes);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:Issuer"],
+                audience: _configuration["JWT:Audience"],
+                expires: DateTime.Now.AddMinutes(ttl),
+                claims: claims,
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+}
