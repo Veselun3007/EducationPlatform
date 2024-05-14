23c84bc95a64e2fd48953da5ca11080ef04e7e0f
Valentyn Renhach <bytethe@gmail.com>
2024-05-14 11:42:17 +0300

    Change delete courseuser behaviour

--- EducationPlatform.Chat/EducationPlatform.Chat/Hubs/ChatHub.cs
+++ EducationPlatform.Chat/EducationPlatform.Chat/Hubs/ChatHub.cs
@@ -35,6 +35,19 @@ namespace EPChat.Web.Hubs
             }
         }
 
+        public async Task EditMessage(int courseId, MessageUpdateDTO message)
+        {
+            var result = await _messageOperation.EditAsync(message);
+            if (result.IsSuccess)
+            {
+                await Clients.Group(courseId.ToString()).SendAsync("EditMedia", result.Value);
+            }
+            else
+            {
+                await Clients.Caller.SendAsync("EditMedia", MessageWrapper.Error(result.Error));
+            }
+        }
+
         public async Task DeleteMessage(int courseId, int messageId, DeleteOptionsEnum deleteOptions)
         {
             var result = await _messageOperation.DeleteAsync(messageId, deleteOptions);
